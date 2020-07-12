ca51ee41b4c24d42733448c1a7576f204a297679
Kartik Goel <goel.kartik39@gmail.com>
2020-07-12 18:00:35 +0530

    Integrated Redux

--- src/components/Level.js
+++ src/components/Level.js
@@ -1,32 +1,72 @@
-import React ,{useEffect} from "react"
+import React ,{useEffect,useState} from "react"
 import {useHistory} from "react-router";
 import {connect} from "react-redux"
 import {getUserDetails} from "../actions/authActions"
+import {getCurrentLevel,updateLevel} from "../actions/levelActions";
 import {withRouter,Redirect} from "react-router-dom"
 import PropTypes from "prop-types"
 
-const Level = ({auth,getUserDetails}) => {
+const Level = ({getCurrentLevel,level,updateLevel}) => {
     
-        getUserDetails();
+    const [answer,setAnswer] = useState("");
+
+    useEffect(() => {
+        getCurrentLevel();
+    })
+    
+    const submitAnswer = (e) => {
+        e.preventDefault();
+        const newAnswer = {
+            answer
+        }
+        console.log(answer);
+        updateLevel(newAnswer);
+    }
     return (
-        !auth.userDetails ? 
-        (<h1> Loading Details </h1>) :
-        (<h1> Hello {auth.userDetails.name} </h1>) 
+       <div>
+         {
+             level.isLoading ? 
+              (<h1> Loading </h1>) :
+              (
+                  <div>
+                      <h1> You are at level {level.levelDetails._id}</h1> 
+                      <br /> <br />
+                      <h1> {level.levelDetails.hint}</h1>
+                      <h1> {level.levelDetails.question}</h1>
+                  </div>
+              )
+         }
+         <input 
+            type ="text" 
+            onChange = {(e) => setAnswer(e.target.value)}
+            name = "answer"
+            value = {answer}
+            placeholder = "Enter your answer"
+         />
+        <button 
+            type="submit"
+            onClick = {(e) => submitAnswer(e)}
+        >
+            Level++?
+        </button>
+       </div>
+       
     )
 }
 
-
 Level.propTypes = {
-    getUserDetails : PropTypes.func.isRequired,
-    auth : PropTypes.object.isRequired,
+    getCurrentLevel : PropTypes.func.isRequired,
+    updateLevel :  PropTypes.func.isRequired,
+    level :  PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state) => ({
-    auth : state.auth
+    level : state.level
 })
 
 const mapDispatchToProps = {
-    getUserDetails
+    getCurrentLevel,
+    updateLevel
 }
 
 export default connect(
