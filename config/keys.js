908fd9345a509b7bee40f53247f033668610c0c3
Kartik Goel <goel.kartik39@gmail.com>
2020-07-18 15:25:28 +0530

    added keys

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+const uri = {
+    MongoURI :"mongodb+srv://hello:hello@cluster0.blov3.gcp.mongodb.net/CryptX?retryWrites=true&w=majority",
+    JWT_SECRET : "helloworld"
+}
+
+module.exports = uri;
