790a1445fe1c28dcecefa1e8438ef87fa5bc0dc2
Kartik Goel <goel.kartik39@gmail.com>
2020-07-18 12:44:31 +0530

    Changed file structure

--- server.js
+++ server.js
@@ -30,17 +30,13 @@ app.use("/",level);
 
 app.use("/public",express.static('public'));
 
-path1 = path.resolve(__dirname,"../","build","index.html");
-console.log(path1);
-//Serve static assets if in production
-if(process.env.NODE_ENV === "production") {
-    console.log("ok");
-    //Set static folder
-    app.use(express.static("../build"));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-    app.get("*",(req,res) => {
-        res.sendFile(path.resolve(__dirname,"../","build","index.html"))
-    })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 const PORT = process.env.PORT || 5000;
