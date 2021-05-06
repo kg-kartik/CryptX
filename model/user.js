dcc2a075f3f98917f37a76122edf2fd9ae27ef88
kg-kartik <goel.kartik39@gmail.com>
2021-05-06 15:34:55 +0530

    Setup email verification

--- model/user.js
+++ model/user.js
@@ -2,32 +2,38 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const userSchema = new schema({
-    name : {
-        type : String,
-        required : true
+    name: {
+        type: String,
+        required: true,
     },
-    university : {
-        type : String,
-        required : true
+    university: {
+        type: String,
+        required: true,
     },
     email: {
-        type : String,
-        required : true
+        type: String,
+        required: true,
     },
-    password : {
-        type : String,
-        required : true
+    password: {
+        type: String,
+        required: true,
     },
-    atLevel : {
-        type : Number,
-        ref : "Level",
-        default : 0
+    atLevel: {
+        type: Number,
+        ref: "Level",
+        default: 0,
     },
-    lastLevelCrackedAt : {
-        type : Date
-    }
-})
+    lastLevelCrackedAt: {
+        type: Date,
+    },
+    isValid: {
+        type: Boolean,
+    },
+    uniqueString: {
+        type: String,
+    },
+});
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -5,128 +5,178 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const requireLogin = require("../middleware/requireLogin");
 const { body, validationResult } = require("express-validator");
+const { route } = require("./levels");
+const { sendMail } = require("../helpers/sendMail");
+const { randomString } = require("../helpers/randString");
 
 require("dotenv").config();
 const secret = process.env.JWT_SECRET;
 console.log("secret", secret);
 
 router.get("/getDetails", requireLogin, (req, res) => {
-	User.findById(req.user._id)
-		.then(user => {
-			res.status(200).json(user);
-		})
-		.catch(err => {
-			res.status(401).json(err);
-		});
+    User.findById(req.user._id)
+        .then((user) => {
+            res.status(200).json(user);
+        })
+        .catch((err) => {
+            res.status(401).json(err);
+        });
 });
 
 router.post(
-	"/signup",
-	[
-		body("name", "Please provide a valid name")
-			.not()
-			.isEmpty(),
-		body("university", "Please provide university name")
-			.not()
-			.isEmpty(),
-		body("email", "Please provide a valid email address").isEmail(),
-		body(
-			"password",
-			"Please provide a password altleast 6 characters long"
-		).isLength({ min: 6 })
-	],
-	(req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
+    "/signup",
+    [
+        body("name", "Please provide a valid name").not().isEmpty(),
+        body("university", "Please provide university name").not().isEmpty(),
+        body("email", "Please provide a valid email address").isEmail(),
+        body(
+            "password",
+            "Please provide a password altleast 6 characters long"
+        ).isLength({ min: 6 }),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-		const { name, university, email, password } = req.body;
+        const { name, university, email, password } = req.body;
 
-		//Checking if the user is already signed up or not
-		User.findOne({
-			email
-		}).then(user => {
-			if (user) {
-				return res.status(400).json({
-					errors: [{ msg: "Email already exists" }]
-				});
-			}
+        //Checking if the user is already signed up or not
+        User.findOne({
+            email,
+        }).then((user) => {
+            if (user) {
+                return res.status(400).json({
+                    errors: [{ msg: "Email already exists" }],
+                });
+            }
 
-			//If not save the new User
-			bcrypt.hash(password, 10).then(hashedPass => {
-				const newUser = new User({
-					name,
-					university,
-					email,
-					password: hashedPass
-				});
+            const uniqueString = randomString();
+            console.log(uniqueString, "string");
+            const isValid = false;
 
-				newUser
-					.save()
-					.then(saveduser => {
-						res.status(200).json(saveduser);
-					})
-					.catch(err => {
-						console.log(err);
-					})
-					.catch(err => {
-						console.log(err);
-					});
-			});
-		});
-	}
+            //If not save the new User
+            bcrypt.hash(password, 10).then((hashedPass) => {
+                const newUser = new User({
+                    isValid,
+                    uniqueString,
+                    name,
+                    university,
+                    email,
+                    password: hashedPass,
+                });
+
+                newUser
+                    .save()
+                    .then((saveduser) => {
+                        res.status(200).json(saveduser);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            });
+
+            sendMail(email, uniqueString);
+            // res.redirect("back");
+        });
+    }
 );
 
 router.post(
-	"/signin",
-	[
-		body("email", "Please provide a valid email address").isEmail(),
-		body("password", "Please provide a password")
-			.not()
-			.isEmpty()
-	],
-	(req, res) => {
-		const { email, password } = req.body;
+    "/signin",
+    [
+        body("email", "Please provide a valid email address").isEmail(),
+        body("password", "Please provide a password").not().isEmpty(),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-		//Checking if the user has registered or not
-		User.findOne({
-			email
-		}).then(user => {
-			if (!user) {
-				return res.status(400).json({
-					errors: [{ msg: "Invalid Credentials" }]
-				});
-			}
+        const { email, password } = req.body;
 
-			bcrypt
-				.compare(password, user.password)
-				.then(isMatch => {
-					if (isMatch) {
-						//If password matches then issue a token depending upon the payload given
-						const token = jwt.sign(
-							{
-								_id: user._id
-							},
-							secret
-						);
+        //Checking if the user has registered or not
 
-						const { _id, email, password } = user;
-						res.json({
-							token,
-							user: { _id, email, password }
-						});
-					} else {
-						res.json({
-							errors: [{ msg: "Invalid Credentials" }]
-						});
-					}
-				})
-				.catch(err => {
-					console.log(err);
-				});
-		});
-	}
+        User.findOne({
+            email,
+        }).then((user) => {
+            if (!user) {
+                return res.status(400).json({
+                    errors: [{ msg: "User not registered" }],
+                });
+            }
+
+            if (!user.isValid) {
+                console.log(user.isValid, "lol");
+                return res.status(400).json({
+                    errors: [{ msg: "Please verify your email." }],
+                });
+            }
+
+            bcrypt
+                .compare(password, user.password)
+                .then((isMatch) => {
+                    if (isMatch) {
+                        //If password matches then issue a token depending upon the payload given
+                        const token = jwt.sign(
+                            {
+                                _id: user._id,
+                            },
+                            secret
+                        );
+
+                        const { _id, email, password } = user;
+                        res.json({
+                            token,
+                            user: { _id, email, password },
+                        });
+                    } else {
+                        res.json({
+                            errors: [{ msg: "Invalid Credentials" }],
+                        });
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        });
+    }
 );
 
+//Verification
+router.get("/verify/:uniqueString", (req, res) => {
+    const { uniqueString } = req.params;
+    //CHeck if any user has the particular string
+    const user = User.findOne({
+        uniqueString,
+    });
+
+    if (user) {
+        //Verify the user
+        User.findOneAndUpdate(
+            {
+                uniqueString,
+            },
+            {
+                isValid: true,
+            },
+            {
+                new: true,
+                runValidators: true,
+            }
+        ).then((user) => {
+            console.log(user);
+        });
+
+        res.redirect("http://localhost:3000/signin");
+    } else {
+        console.log("User not found");
+        res.json("User not found");
+    }
+});
 module.exports = router;
