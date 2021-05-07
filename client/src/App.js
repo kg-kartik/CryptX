aa004493aac1e454bcd49bd98b0351ff09060e51
kg-kartik <goel.kartik39@gmail.com>
2021-05-07 10:24:06 +0530

    Minor styling changes

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import jwt_decode from "jwt-decode";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import ProtectedRoute from "./commons/ProtectedRoute";
 import PublicRoute from "./commons/PublicRoute";
+import Team from "./components/Team";
 
 // export const appHistory = createBrowserHistory();
 
@@ -27,72 +28,78 @@ import PublicRoute from "./commons/PublicRoute";
 const jwtToken = localStorage.getItem("token");
 
 if (jwtToken) {
-	//Set authorization token header
-	setAuthToken(jwtToken);
+    //Set authorization token header
+    setAuthToken(jwtToken);
 
-	//Decode the access token to get user info
-	const decodedToken = jwt_decode(jwtToken);
+    //Decode the access token to get user info
+    const decodedToken = jwt_decode(jwtToken);
 
-	//Dispatch the token for seeting the user to be authenticated
-	store.dispatch(setCurrentUser(decodedToken));
+    //Dispatch the token for seeting the user to be authenticated
+    store.dispatch(setCurrentUser(decodedToken));
 
-	//checking for any expiry date
-	const currentTime = new Date() / 1000;
-	if (decodedToken.exp < currentTime) {
-		//Logout current User
-		store.dispatch(logoutUser());
-		console.log("User has been logged out");
-	}
+    //checking for any expiry date
+    const currentTime = new Date() / 1000;
+    if (decodedToken.exp < currentTime) {
+        //Logout current User
+        store.dispatch(logoutUser());
+        console.log("User has been logged out");
+    }
 }
 
 const App = () => {
-	return (
-		<Provider store={store}>
-			<Router>
-				<Alert />
-				<Switch>
-					<PublicRoute
-						restricted={false}
-						exact
-						path="/"
-						component={Home}
-					/>
-					<PublicRoute
-						restricted={true}
-						exact
-						path="/signup"
-						component={SignUpNew}
-					/>
-					<PublicRoute
-						restricted={true}
-						exact
-						path="/signupold"
-						component={SignUp}
-					/>
-					<PublicRoute
-						restricted={true}
-						exact
-						path="/signin"
-						component={SignInNew}
-					/>
-					<PublicRoute
-						restricted={true}
-						exact
-						path="/signinold"
-						component={SignIn}
-					/>
-					<PublicRoute
-						restricted={false}
-						exact
-						path="/leaderboard"
-						component={Leaderboard}
-					/>
-					<ProtectedRoute exact path="/level" component={LevelNew} />
-					<ProtectedRoute exact path="/levelOld" component={Level} />
-				</Switch>
-			</Router>
-		</Provider>
-	);
+    return (
+        <Provider store={store}>
+            <Router>
+                <Alert />
+                <Switch>
+                    <PublicRoute
+                        restricted={false}
+                        exact
+                        path="/"
+                        component={Home}
+                    />
+                    <PublicRoute
+                        restricted={true}
+                        exact
+                        path="/signup"
+                        component={SignUpNew}
+                    />
+                    <PublicRoute
+                        restricted={true}
+                        exact
+                        path="/signupold"
+                        component={SignUp}
+                    />
+                    <PublicRoute
+                        restricted={true}
+                        exact
+                        path="/signin"
+                        component={SignInNew}
+                    />
+                    <PublicRoute
+                        restricted={true}
+                        exact
+                        path="/signinold"
+                        component={SignIn}
+                    />
+                    <PublicRoute
+                        restricted={false}
+                        exact
+                        path="/leaderboard"
+                        component={Leaderboard}
+                    />
+                    <PublicRoute
+                        restricted={true}
+                        exact
+                        path="/team"
+                        component={Team}
+                    />
+                    <ProtectedRoute exact path="/level" component={LevelNew} />
+                    <ProtectedRoute exact path="/levelOld" component={Level} />
+                </Switch>
+            </Router>
+        </Provider>
+    );
 };
 
 export default App;
--- client/src/layouts/Navbar.js
+++ client/src/layouts/Navbar.js
@@ -8,94 +8,104 @@ import theme from "../styles/themes";
 import styled from "styled-components";
 
 const Nav = styled.nav`
-	background:${theme.mainBackground} !important;
-`
+    background: ${theme.mainBackground} !important;
+`;
 
 const Navbar = ({ auth: { isAuthenticated, loading } }) => {
-	const logout = () => {
-		localStorage.removeItem("token");
-		window.location.reload();
-	};
+    const logout = () => {
+        localStorage.removeItem("token");
+        window.location.reload();
+    };
 
-	return (
-		<div id="nav-bar">
-			<Nav className="navbar navbar-expand-lg navbar-dark">
-				<button
-					class="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarNav"
-					aria-controls="navbarNav"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span class="navbar-toggler-icon"></span>
-				</button>
+    return (
+        <div id="nav-bar">
+            <Nav className="navbar navbar-expand-lg navbar-dark">
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-				<div className="collapse navbar-collapse" id="navbarNav">
-					{!isAuthenticated ? (
-						<ul className="navbar-nav ml-auto">
-							<li className="nav-item">
-								<Link className="nav-link" to="/">
-									HOME
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link className="nav-link" to="/signup">
-									SIGNUP
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link className="nav-link" to="/signin">
-									SIGNIN
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link className="nav-link" to="/leaderboard">
-									LEADERBOARD
-								</Link>
-							</li>
-						</ul>
-					) : (
-						<ul className="navbar-nav ml-auto">
-							<li className="nav-item">
-								<Link className="nav-link" to="/">
-									HOME
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link className="nav-link" to="/level">
-									PLAY
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link className="nav-link" to="/leaderboard">
-									LEADERBOARD
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link
-									className="nav-link logout"
-									onClick={() => logout()}
-								>
-									LOGOUT
-								</Link>
-							</li>
-						</ul>
-					)}
-				</div>
-			</Nav>
-		</div>
-	);
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    {!isAuthenticated ? (
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">
+                                    HOME
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/signup">
+                                    SIGNUP
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/signin">
+                                    SIGNIN
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/leaderboard">
+                                    LEADERBOARD
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/team">
+                                    TEAM
+                                </Link>
+                            </li>
+                        </ul>
+                    ) : (
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">
+                                    HOME
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/level">
+                                    PLAY
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/leaderboard">
+                                    LEADERBOARD
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/team">
+                                    TEAM
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    className="nav-link logout"
+                                    onClick={() => logout()}
+                                >
+                                    LOGOUT
+                                </Link>
+                            </li>
+                        </ul>
+                    )}
+                </div>
+            </Nav>
+        </div>
+    );
 };
 
 Navbar.propTypes = {
-	logoutUser: PropTypes.func.isRequired,
-	auth: PropTypes.object.isRequired
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-	auth: state.auth
+const mapStateToProps = (state) => ({
+    auth: state.auth,
 });
 
 export default connect(mapStateToProps, { logoutUser })(Navbar);
