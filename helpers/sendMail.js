a84cf7cf98c1bc912852483753fe7a3604f43c70
kg-kartik <goel.kartik39@gmail.com>
2022-04-07 23:34:28 +0530

    Shifted from nodemailer to sendgrid

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,35 +1,29 @@
-const nodemailer = require("nodemailer");
 require("dotenv").config();
+const sendgrid = require('@sendgrid/mail');
 
-const emailMain = process.env.emailMain;
-const password = process.env.password;
-
-module.exports.sendMail = (email, uniqueString) => {
-    var transport = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-            email: emailMain,
-            password,
-        },
-    });
-
-    console.log("transport", transport);
-    var mailOptions;
-    let sender = "Cryptx";
-    mailOptions = {
-        from: sender,
+module.exports.sendMail = (email,uniqueString) => {
+    const SENDGRID_API_KEY = process.env.API_KEY;
+    
+    sendgrid.setApiKey(SENDGRID_API_KEY);
+    
+    const msg = {
         to: email,
+        // Change to your recipient
+        from: 'cryptxmuj@gmail.com',
+        // Change to your verified sender
         subject: "Email Verification",
         html: `Thank you for registering on CryptX 
-            .Please verify your email <a href=https://cryptx.azurewebsites.net/user/verify/${uniqueString}>here</a>`,
-    };
+        .Please verify your email <a clicktracking="off" href=http://localhost:5000/user/verify/${uniqueString}>here</a>`,
+        text:"hello"
+    }
+    
+    sendgrid
+        .send(msg)
+        .then((resp) => {
+            console.log('Email sent\n', resp)
+        })
+        .catch((error) => {
+            console.error(error)
+    })
+}
 
-    transport.sendMail(mailOptions, (err, response) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Message Sent");
-            console.log(response, "response");
-        }
-    });
-};
