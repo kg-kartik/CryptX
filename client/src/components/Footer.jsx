3fa5e04e5bc55817dbd4d93ef8f928c1e823d700
boidushya <boidushyabhattacharya@gmail.com>
2022-04-13 02:47:15 +0530

    fix: added ee

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,177 +1,170 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import theme from '../styles/themes';
+import theme from "../styles/themes";
 import logo from "../static/logo.png";
 
 const Section = styled.section`
-	min-height: 15vh;
-    margin-top:20vh;
-	background:${theme.cardBackground};
-    @media (max-width: 767px) {
-		min-height:5vh;
-	}
-`
+  min-height: 15vh;
+  margin-top: 20vh;
+  background: ${theme.cardBackground};
+  @media (max-width: 767px) {
+    min-height: 5vh;
+  }
+`;
 
 const Container = styled.div`
-	display:flex;
-	flex-wrap: wrap;
-	flex-direction:row;
-	align-items:stretch	;
-	justify-content:space-between;
-	@media (max-width: 767px) {
-		justify-content:center;
-		flex-direction:column;
-	}
-`
-
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: stretch;
+  justify-content: space-between;
+  @media (max-width: 767px) {
+    justify-content: center;
+    flex-direction: column;
+  }
+`;
 
 const ParentWrapper = styled.div`
-	display:flex;
-	flex-direction:column;
-	justify-content:space-around;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
 
 const MetaWrapper = styled.div`
-	display: flex;
-	padding: 2rem;
-	@media (max-width: 767px) {
-		justify-content:center;
-		padding:0;
-	}
-`
+  display: flex;
+  padding: 2rem;
+  @media (max-width: 767px) {
+    justify-content: center;
+    padding: 0;
+  }
+`;
 
 const LinksWrapper = styled.ul`
-	display: flex;
-	text-align:center;
-	margin-top:5vh;
-	li:first-child {
-		list-style: none;
-	}
-	li:last-child {
-		padding: 0;
-	}
-	@media (max-width: 767px) {
-		margin-top:1rem;
-		padding: 0 !important;
-	}
-`
+  display: flex;
+  text-align: center;
+  margin-top: 5vh;
+  li:first-child {
+    list-style: none;
+  }
+  li:last-child {
+    padding: 0;
+  }
+  @media (max-width: 767px) {
+    margin-top: 1rem;
+    padding: 0 !important;
+  }
+`;
 
 const Links = styled.li`
-	cursor: pointer;
-	letter-spacing:0;
-	color: lightgray !important;
-	font-size: 1.2rem;
-	// padding: 0 1rem 0 0;
-	margin: 0 1rem 0 1rem;
-	@media (max-width: 767px) {
-		margin: 0 0.5rem 1.5rem 0.5rem;
-		list-style:none;
-		font-size: 1.1em;
-	}
-	&:hover{
-		text-decoration:underline;
-	}
-`
-
+  cursor: pointer;
+  letter-spacing: 0;
+  color: lightgray !important;
+  font-size: 1.2rem;
+  // padding: 0 1rem 0 0;
+  margin: 0 1rem 0 1rem;
+  @media (max-width: 767px) {
+    margin: 0 0.5rem 1.5rem 0.5rem;
+    list-style: none;
+    font-size: 1.1em;
+  }
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const SocialWrapper = styled.div`
-	padding-bottom: 2rem;
-	width:100%;
-	display:flex;
-	justify-content:center;
-	@media (max-width: 767px) {
-		justify-content:center;
-	}
-`
+  padding-bottom: 2rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  @media (max-width: 767px) {
+    justify-content: center;
+  }
+`;
 
 const Icon = styled.i`
-	margin: auto 1rem;
-	cursor: pointer;
-	color: #909090;
-	transition: all 0.2s ease;
-	&:hover{
-		color: white;
-	}
-`
+  margin: auto 1rem;
+  cursor: pointer;
+  color: #909090;
+  transition: all 0.2s ease;
+  &:hover {
+    color: white;
+  }
+`;
 
 const IconContainer = styled.a`
-	font-size:2rem;
-`
+  font-size: 2rem;
+`;
 
 const BrandHeader = styled.div`
-	padding:1rem;
-    width: 30%;
-    display: flex;
-    align-items: center;
-	@media (max-width: 767px) {
-		width: 100%;
-		padding:1rem 1rem 0;
-	}
-`
+  padding: 1rem;
+  width: 20%;
+  display: flex;
+  align-items: center;
+  @media (max-width: 767px) {
+    width: 100%;
+    padding: 1rem 1rem 0;
+  }
+`;
 
 const Image = styled.img`
-	max-width:120%;
-	@media (max-width: 767px) {
-		max-width: 100%;
-	}
-`
+  max-width: 120%;
+  @media (max-width: 767px) {
+    max-width: 100%;
+  }
+`;
 
 const Footer = () => {
-	const linkList = [
-        {
-			name: "Home",
-			href: "/",
-		},
-		{
-			name: "About",
-			href: "/#about",
-		},
-		{
-			name: "Rules",
-			href: "/#rules",
-		},
-		{
-			name: "Team",
-			href: "/team",
-		}
-	]
-	return (
-		<Section id="contact">
-			<Container>
-				<BrandHeader>
-					<Image src={logo}/>
-				</BrandHeader>
-				<ParentWrapper>
+  const linkList = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "About",
+      href: "/#about",
+    },
+    {
+      name: "Rules",
+      href: "/#rules",
+    },
+    {
+      name: "Team",
+      href: "/team",
+    },
+  ];
+  return (
+    <Section id="contact">
+      <Container>
+        <BrandHeader>
+          <Image src={logo} />
+        </BrandHeader>
+        <ParentWrapper>
+          <MetaWrapper>
+            <LinksWrapper>
+              {linkList.map((obj, i) => (
+                <a href={obj.href}>
+                  <Links key={i}>{obj.name}</Links>
+                </a>
+              ))}
+            </LinksWrapper>
+          </MetaWrapper>
+          {/* <FlexBreak /> */}
+          <SocialWrapper>
+            <IconContainer href="https://www.instagram.com/randomizemuj/">
+              <Icon className="fab fa-instagram" />
+            </IconContainer>
+            <IconContainer href="https://github.com/Randomize-MUJ/">
+              <Icon className="fab fa-github" />
+            </IconContainer>
+            <IconContainer href="https://www.linkedin.com/company/randomizemuj/">
+              <Icon className="fab fa-linkedin-in" />
+            </IconContainer>
+          </SocialWrapper>
+        </ParentWrapper>
+      </Container>
+    </Section>
+  );
+};
 
-				<MetaWrapper>
-					<LinksWrapper>
-						{
-							linkList.map((obj,i) => (
-								<a  href={obj.href}>
-									<Links key={i}>
-										{obj.name}
-									</Links>
-								</a>
-							))
-						}
-					</LinksWrapper>
-				</MetaWrapper>
-				{/* <FlexBreak /> */}
-				<SocialWrapper>
-					<IconContainer href="https://www.instagram.com/randomizemuj/">
-						<Icon className="fab fa-instagram" />
-					</IconContainer>
-					<IconContainer href="https://github.com/Randomize-MUJ/">
-						<Icon className="fab fa-github" />
-					</IconContainer>
-					<IconContainer href="https://www.linkedin.com/company/randomizemuj/">
-						<Icon className="fab fa-linkedin-in" />
-					</IconContainer>
-				</SocialWrapper>
-			</ParentWrapper>
-		</Container>
-	</Section>
-	)
-}
-
-export default Footer
+export default Footer;
---
+++ client/src/components/WW.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const WW = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "grid",
+        placeItems: "center",
+      }}
+    >
+      <p
+        style={{
+          fontSize: "1.25rem",
+          color: "#fff",
+          padding: "2rem 3rem",
+          textAlign: "justify",
+          width: "50%",
+        }}
+      >
+        My name is Walter Hartwell White. I live at 1701 Mountain Road NW,
+        Albuquerque, New Mexico, 87104. To all law enforcement entities, this is
+        not an admission of guilt. I am speaking to my family now. Skyler, you
+        are the love of my life. I hope you know that. Walter Jr., you're my big
+        man. There are going to be some things that you'll come to learn about
+        me in the next few days. But just know that no matter how it may look, I
+        only had you in my heart. Goodbye.
+      </p>
+    </div>
+  );
+};
+
+export default WW;
